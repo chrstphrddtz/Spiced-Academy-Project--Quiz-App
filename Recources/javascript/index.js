15edbe1041c0dc1c8b837d6a48e7d33d7f913333
christoph.raddatz <hello@chrstphrddtz.com>
2023-02-21 18:03:00 +0100

    added new changes to index.html + index.css

---
+++ Recources/javascript/index.js
@@ -0,0 +1,34 @@
+// const mainElement = document.querySelector('[data-js="main"]');
+
+const changeBookmarkIcon01 = document.querySelector('[data-js="icon01"]');
+changeBookmarkIcon01.addEventListener("click", () => {
+    changeBookmarkIcon01.classList.toggle("filled");
+});
+
+const changeBookmarkIcon02 = document.querySelector('[data-js="icon02"]');
+changeBookmarkIcon02.addEventListener("click", () => {
+    changeBookmarkIcon02.classList.toggle("filled");
+});
+
+const changeBookmarkIcon03 = document.querySelector('[data-js="icon03"]');
+changeBookmarkIcon03.addEventListener("click", () => {
+    changeBookmarkIcon03.classList.toggle("filled");
+});
+
+const changeBookmarkIcon04 = document.querySelector('[data-js="icon04"]');
+changeBookmarkIcon04.addEventListener("click", () => {
+    changeBookmarkIcon04.classList.toggle("filled");
+});
+
+const changeBookmarkIcon05 = document.querySelector('[data-js="icon05"]');
+changeBookmarkIcon05.addEventListener("click", () => {
+    changeBookmarkIcon05.classList.toggle("filled");
+});
+
+const changeBookmarkIcon06 = document.querySelector('[data-js="icon06"]');
+changeBookmarkIcon06.addEventListener("click", () => {
+    changeBookmarkIcon06.classList.toggle("filled");
+});
+
+
+
